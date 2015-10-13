ed26e5abf7f3f0d5fcc15eb0505bf1fd154c81b0
Alejandro Isaza <aisaza@venturemedia.com>
2015-10-13 12:54:06 -0700

    Create project

---
+++ BrainCore/BrainCore.h
@@ -0,0 +1,11 @@
+//  Copyright © 2015 Venture Media Labs. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrainCore.
+FOUNDATION_EXPORT double BrainCoreVersionNumber;
+
+//! Project version string for BrainCore.
+FOUNDATION_EXPORT const unsigned char BrainCoreVersionString[];
+
+
---
+++ BrainCoreTests/BrainCoreTests.swift
@@ -0,0 +1,7 @@
+//  Copyright © 2015 Venture Media Labs. All rights reserved.
+
+import XCTest
+@testable import BrainCore
+
+class BrainCoreTests: XCTestCase {
+}
